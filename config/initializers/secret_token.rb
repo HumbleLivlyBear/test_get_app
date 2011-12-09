753a907073b4fc63f095406a9a22ae7c339e1c16
HumbleLivlyBear <kerlichung@gmail.com>
2011-12-09 17:13:17 +0800

    GoGoGo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGetApp::Application.config.secret_token = '66b65596aeae803d6c101ff44df7dccd79ff9810f278cc05e1c94add00456843a340ed3dcbf917ffe517cad95f69934804ea37e76830889877c4f2337886b4ca'
