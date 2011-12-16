6ba4f4de68040fd1f09b4dbd0af203e7c1c8d068
HumbleLivlyBear <kerlichung@gmail.com>
2011-12-16 16:29:36 +0800

    sdfdsf

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,16 @@
 class LocationsController < ApplicationController
   def get
+    
+    =begin
     @location = 
     { 
       [:name=>"Taipei 101" , :lat => "25.034264", :lng => "121.564064"]
       [:name=>"Yangming" , :lat => "25.157327", :lng => "121.547999"]
       [:name=>"Taroku"  , :lat => "24.156348", :lng => "121.621263" ]
     
-    }   
+    }
+    =end
+    @location = {:lat => "25.034264", :lng => "121.564064"}
     respond_to do |format|
       format.html
       format.json {
