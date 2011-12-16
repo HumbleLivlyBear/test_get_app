55f7e84168de45a8d325220efb6f10ce7bac0e93
HumbleLivlyBear <kerlichung@gmail.com>
2011-12-16 16:09:01 +0800

    Change Datas

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,12 @@
 class LocationsController < ApplicationController
   def get
-    @location = { :lat => "25.034264", :lng => "121.564064"}   
+    @location = 
+    { 
+      [:name=>Taipei 101 => {:lat => "25.034264", :lng => "121.564064"}]
+      [:name=>Yangming => {:lat => "25.157327", :lng => "121.547999"}]
+      [:name=>Taroku  => {:lat => "24.156348", :lng => "121.621263"}]
+    
+    }   
     respond_to do |format|
       format.html
       format.json {
